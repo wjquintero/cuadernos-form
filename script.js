db60aa4a2be5aa0b9ce38c82edd97db707540540
wjquintero <williamqq@gmail.com>
2026-01-31 12:36:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function fetchMaxImages() {
   fetch(WORKER_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ action: 'getMaxImagesPerUpload' })
+    body: JSON.stringify({ action: 'getMaxImagesInUpload' })
   })
   .then(res => res.json())
   .then(res => {
